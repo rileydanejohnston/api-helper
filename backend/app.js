eb261e14c05146a552390f7cb64951906e426e6c
Riley Johnston <rileydanejohnston@gmail.com>
2021-12-23 12:47:56 -0600

    feat: add mongoose to app.js

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,14 @@
 npm install express
 npm install nodemon -D
 npm init
+        i don't think this is necessary?
+      brew tap mongodb/brew
+      brew install mongodb-community
+npm i mongoose
+mongod - starts database
 */
-
 const express = require('express');
+const mongoose = require('mongoose');
 
 // create port variable
 const { PORT = 3000 } = process.env;
@@ -12,7 +17,10 @@ const { PORT = 3000 } = process.env;
 // create application variable
 const app = express();
 
-// middleware so all requests come through as JSON
+// create db with mongoose
+mongoose.connect('mongodb://localhost:27017/api-helper');
+
+// middleware function to parse incoming requests as JSON
 app.use(express.json());
 
 // listen for requests on our port variable
